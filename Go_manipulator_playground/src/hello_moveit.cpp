910afb65ecde66dca533976374fa1be1e02bc587
root <root@DESKTOP-I078OET>
2024-10-30 22:56:26 +0900

    Added manipulator moveit_config

---
+++ Go_manipulator_playground/src/hello_moveit.cpp
@@ -0,0 +1,76 @@
+#include <memory>
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <chrono>
+#include <thread>
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto const node = std::make_shared<rclcpp::Node>(
+    "hello_moveit",
+    rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
+  );
+
+  auto const logger = rclcpp::get_logger("hello_moveit");
+
+  // Create the MoveIt MoveGroup Interface for the arm
+  using moveit::planning_interface::MoveGroupInterface;
+  auto move_group_interface = MoveGroupInterface(node, "arm");
+
+  auto const target_pose = []{
+    geometry_msgs::msg::Pose msg;
+    msg.orientation.x = 0.0;
+    msg.orientation.y = 0.0;
+    msg.orientation.z = 0.0;
+    msg.orientation.w = 1.0;
+    msg.position.x = -0.1;
+    msg.position.y = -0.2;
+    msg.position.z = 0.6;
+    return msg;
+  }();
+  move_group_interface.setPoseTarget(target_pose);
+  move_group_interface.setGoalJointTolerance(0.001);
+  move_group_interface.setGoalOrientationTolerance(0.001);
+
+  auto const [success, plan] = [&move_group_interface]{
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
+
+  if(success) {
+    move_group_interface.execute(plan);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  } else {
+    RCLCPP_ERROR(logger, "Planning failed for the arm!");
+  }
+
+  auto gripper_interface = MoveGroupInterface(node, "gripper");
+
+  gripper_interface.setNamedTarget("close");
+  if (gripper_interface.move()) {
+    RCLCPP_INFO(logger, "Gripper closed successfully");
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  } else {
+    RCLCPP_ERROR(logger, "Failed to close the gripper");
+  }
+
+  move_group_interface.setNamedTarget("ready");
+  if (move_group_interface.move()) {
+    RCLCPP_INFO(logger, "Arm moved back to home position");
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    gripper_interface.setNamedTarget("open");
+    if (gripper_interface.move()) {
+      RCLCPP_INFO(logger, "Gripper opened successfully");
+    } else {
+      RCLCPP_ERROR(logger, "Failed to open the gripper");
+    }
+
+  } else {
+    RCLCPP_ERROR(logger, "Failed to move the arm back to home position");
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
